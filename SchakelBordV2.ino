31e5d38318c778d9df4ad837b9ac57b3f03f65ab
Rob Antonisse <robantonisse@gmail.com>
2024-04-25 10:44:46 +0200

    Add project files.

---
+++ SchakelBordV2.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SchakelBordV2.ino
+ Created:	4/25/2024 10:43:35 AM
+ Author:	gebruiker
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
