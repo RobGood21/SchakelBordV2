bbea5dfdb8bb76a114ad1850270a5888bf059931
Rob Antonisse <robantonisse@gmail.com>
2024-04-25 10:46:28 +0200

    Start project

--- SchakelBordV2.ino
+++ SchakelBordV2.ino
@@ -1,15 +1,20 @@
 /*
  Name:		SchakelBordV2.ino
  Created:	4/25/2024 10:43:35 AM
- Author:	gebruiker
+ Author:	Rob Antonisse
+
+ Schakelbord, een op arduino gebaseerde DCC centrale specifiek voor aansturen van accessories als wissels en seinen met DCC. 
+
+
+
 */
 
-// the setup function runs once when you press reset or power the board
+
 void setup() {
 
 }
 
-// the loop function runs over and over again until power down or reset
+
 void loop() {
   
 }
